fdeefdafc3e6e775784095a07b101ef7b55d7735
JornaldRem <aungkokolin23397.com>
2022-07-16 12:37:58 +0630

    'upload'

---
+++ partner_contact_birthdate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Contact's birthdate",
+    "version": "15.0.1.0.0",
+    "author": "Aung Ko Ko Lin(aungkokolin23397@gmail.com)",
+    "category": "Customer Relationship Management",
+    "website": "",
+    "depends": ["base","contacts"],
+    "data": [
+        "data/mail_data.xml",
+        "data/send_mail.xml",
+        "views/res_partner.xml"
+        ],
+    "license": "AGPL-3",
+    "installable": True,
+}
